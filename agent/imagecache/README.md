0bd28f72b517ea457fa3a77ad5e7aad2fa85feec
schwarzeni <nzyalj@126.com>
2022-01-03 19:33:20 +0800

    imagecachecontroller 实现功能：定时移除未及时上传心跳的节点信

--- agent/imagecache/README.md
+++ agent/imagecache/README.md
@@ -1,9 +1,9 @@
 未实现：
 
 - 实现 manifest 缓存，以及其它节点获取 manifest 缓存的功能 [finish]
-- 代理下载 layer 请求时策略的实现（参考飞书流程图，目前仅仅是随机选取一个存在 layer 的节点）
-- 使用 cache.Layer 来验证 layer 是否存在，同时在代理的时候更新
-- imagecachecontroller 实现功能：定时移除未及时上传心跳的节点信息
+- 代理下载 layer 请求时策略的实现（参考飞书流程图，目前仅仅是随机选取一个存在 layer 的节点）[finish]
+- ~~使用 cache.Layer 来验证 layer 是否存在，同时在代理的时候更新~~ 
+- imagecachecontroller 实现功能：定时移除未及时上传心跳的节点信息 [finish]
 - 重构一下节点数据是由那个结构体记录的，from cache.metric or response or cache.nodeinfos
 - 研究 response 需要返回那些标准性的 header （目前阶段还需要请求一下远端的仓库）
 - 实现 manifest 代理策略（仅在断网的情况下使用？）
--- imagecachecontroller/noderegion.go
+++ imagecachecontroller/noderegion.go
@@ -2,23 +2,30 @@ package imagecachecontroller
 
 import (
 	"sync"
+	"time"
 
 	"github.com/schwarzeni/k8senhance/pkg/model"
 )
 
 var (
 	// TODO: 优化此处的数据结构
-	nodeRegionDB     = map[string]map[string]*model.ImageCacheAgentHealthReqDTO{}
+	nodeRegionDB     = map[string]map[string]NodeRegionInfo{}
 	nodeRegionDBLock sync.RWMutex
+	timeoutDuration  = time.Second * 10
 )
+
+type NodeRegionInfo struct {
+	data       *model.ImageCacheAgentHealthReqDTO
+	lastUpdate time.Time
+}
 
 func setNodeRegionDB(nodeinfo *model.ImageCacheAgentHealthReqDTO) error {
 	nodeRegionDBLock.Lock()
 	defer nodeRegionDBLock.Unlock()
 	if _, ok := nodeRegionDB[nodeinfo.Region]; !ok {
-		nodeRegionDB[nodeinfo.Region] = map[string]*model.ImageCacheAgentHealthReqDTO{}
+		nodeRegionDB[nodeinfo.Region] = map[string]NodeRegionInfo{}
 	}
-	nodeRegionDB[nodeinfo.Region][nodeinfo.NodeID] = nodeinfo
+	nodeRegionDB[nodeinfo.Region][nodeinfo.NodeID] = NodeRegionInfo{data: nodeinfo, lastUpdate: time.Now()}
 	return nil
 }
 
@@ -27,8 +34,12 @@ func getAllNodesInRegion(region string) ([]*model.ImageCacheAgentHealthReqDTO, e
 	defer nodeRegionDBLock.RUnlock()
 	var res []*model.ImageCacheAgentHealthReqDTO
 	nodes := nodeRegionDB[region]
-	for _, v := range nodes {
-		res = append(res, v)
+	for key, v := range nodes {
+		if time.Now().Sub(v.lastUpdate) < timeoutDuration {
+			res = append(res, v.data)
+		} else {
+			delete(nodes, key)
+		}
 	}
 	return res, nil
 }
